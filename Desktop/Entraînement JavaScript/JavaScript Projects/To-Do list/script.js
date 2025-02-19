28f43f857511b2beaf7bde570351fa9d4f4449f3
AmaraABC <amara-noah.gakou@efrei.net>
2025-02-19 17:20:57 +0100

    Ajout de lignes de code pour tester le lancement d'un serveur sur un port

---
+++ Desktop/Entraînement JavaScript/JavaScript Projects/To-Do list/script.js
@@ -0,0 +1,37 @@
+const newInput = document.getElementById("tasks");
+const newOutput = document.getElementById("tasks-output");
+
+const actualDate = new Date();
+const myDate = actualDate.toLocaleString('fr-FR',{
+    weekday: 'short',
+    day: 'numeric',
+    month: '2-digit',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+})
+
+function addTasks() {
+    if (newInput.value == '') {
+        alert("Vous devez au moins écrire quelque chose...");
+    }
+
+    else {
+        const newTask = document.createElement("span");
+        newTask.classList.add("active-tasks");
+        newTask.innerHTML = newInput.value + " - " + myDate;
+        newOutput.appendChild(newTask);
+        const newButton = document.createElement("button");
+        newButton.innerHTML = "<button style='padding: .3em;'>Supprimer</button>";
+        newButton.classList.add("eraser");
+        newTask.appendChild(newButton);
+        
+
+        newButton.addEventListener("click", function() {
+            if (newTask.classList.contains("active-tasks") == true) {
+                newOutput.removeChild(newTask);
+            }
+        })
+    }
+}
