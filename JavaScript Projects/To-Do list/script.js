0bd7e4b11ad4f6d49a79e7612ded342850a43a3d
AmaraABC <amara-noah.gakou@efrei.net>
2025-03-13 23:47:58 +0100

    Amélioration du fichier JS pour l'ajout de tâches dans la to-do list

--- JavaScript Projects/To-Do list/script.js
+++ JavaScript Projects/To-Do list/script.js
@@ -1,8 +1,11 @@
-const newInput = document.getElementById("tasks");
-const newOutput = document.getElementById("tasks-output");
+// 3 constantes pour représenter l'intitulé de la tâche, son résumé et son ajout
+const input = document.getElementById("task");
+const comments = document.getElementById("comments");
+const output = document.getElementById("tasks-output");
 
+// Définition de la date
 const actualDate = new Date();
-const myDate = actualDate.toLocaleString('fr-FR',{
+const myDate = actualDate.toLocaleString('fr-FR', {
     weekday: 'short',
     day: 'numeric',
     month: '2-digit',
@@ -12,17 +15,43 @@ const myDate = actualDate.toLocaleString('fr-FR',{
     second: 'numeric'
 })
 
+// Fonction d'incrémentation pour différencier les identifiants de chaque tâche
+let idIncrement = 1;
+
+function newIntForId() {
+    return idIncrement++;
+}
+
 function addTasks() {
-    if (newInput.value == '') {
+    if (input.value == '') {
         alert("Vous devez au moins écrire quelque chose...");
     }
 
     else {
-        const newTask = document.createElement("span");
-        newTask.classList.add("active-tasks");
-        newTask.innerHTML = newInput.value + ` - Tâche ajoutée le ${myDate}`;
-        newOutput.appendChild(newTask);
-        const newButton = document.createElement("button");
+        // Définition d'une div et de sa classe
+        const firstDiv = document.createElement('div');
+        firstDiv.style.marginBottom = "1vw";
+        firstDiv.classList.add("active-tasks");
+
+        // Ajout de la tâche saisie auparavant
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute("id", `task-check-${idIncrement}`);
+        checkbox.setAttribute("name", `task-check-${idIncrement}`);
+        checkbox.setAttribute("type", "checkbox");
+        firstDiv.appendChild(checkbox);
+
+        const intitule = document.createElement('label');
+        intitule.classList.add("task-name");
+        intitule.setAttribute("for", `task-check-${idIncrement}`);
+        intitule.innerText = input.value;
+        intitule.style.marginLeft = '2vw';
+        intitule.style.fontSize = '4vh';
+        firstDiv.appendChild(intitule);
+
+        output.appendChild(firstDiv);
+        newIntForId();
+
+        /*newOutput.appendChild(newDiv);
         newButton.innerHTML = "<button style='padding: .3em;'>Supprimer</button>";
         newButton.classList.add("eraser");
         newTask.appendChild(newButton);
@@ -32,6 +61,6 @@ function addTasks() {
             if (newTask.classList.contains("active-tasks") == true) {
                 newOutput.removeChild(newTask);
             }
-        })
+        })*/
     }
 }
