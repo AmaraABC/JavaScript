620680200d3bcee42014c106630f600d5f8ac1c2
AmaraABC <amara-noah.gakou@efrei.net>
2025-03-16 13:08:19 +0100

    Amélioration du fichier JS

--- JavaScript Projects/To-Do list/script.js
+++ JavaScript Projects/To-Do list/script.js
@@ -2,18 +2,26 @@
 const input = document.getElementById("task");
 const comments = document.getElementById("comments");
 const output = document.getElementById("tasks-output");
+const button = document.getElementById("submit-button");
 
-// Définition de la date
-const actualDate = new Date();
-const myDate = actualDate.toLocaleString('fr-FR', {
-    weekday: 'short',
-    day: 'numeric',
-    month: '2-digit',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
-})
+// Définition de la date et du temps
+function displayDateTime() {
+    const actualDate = new Date();
+    const myDate = actualDate.toLocaleString('fr-FR', {
+        weekday: 'short',
+        day: 'numeric',
+        month: '2-digit',
+        year: 'numeric'
+    });
+
+    const myTime = actualDate.toLocaleString('fr-FR', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    });
+
+    return `Date de création : ${myDate}, à ${myTime}`
+}
 
 // Fonction d'incrémentation pour différencier les identifiants de chaque tâche
 let idIncrement = 1;
@@ -22,45 +30,102 @@ function newIntForId() {
     return idIncrement++;
 }
 
+// Fonction pour l'ajout d'une nouvelle tâche
 function addTasks() {
     if (input.value == '') {
-        alert("Vous devez au moins écrire quelque chose...");
+        alert("Vous devez au moins écrire quelque chose dans l'intitulé...");
     }
 
     else {
         // Définition d'une div et de sa classe
-        const firstDiv = document.createElement('div');
-        firstDiv.style.marginBottom = "1vw";
-        firstDiv.classList.add("active-tasks");
+        const mainArticle = document.createElement('div');
+        mainArticle.style.marginBottom = "1vw";
+        mainArticle.classList.add("active-tasks");
 
         // Ajout de la tâche saisie auparavant
         const checkbox = document.createElement('input');
         checkbox.setAttribute("id", `task-check-${idIncrement}`);
         checkbox.setAttribute("name", `task-check-${idIncrement}`);
         checkbox.setAttribute("type", "checkbox");
-        firstDiv.appendChild(checkbox);
+        mainArticle.appendChild(checkbox);
 
         const intitule = document.createElement('label');
         intitule.classList.add("task-name");
         intitule.setAttribute("for", `task-check-${idIncrement}`);
         intitule.innerText = input.value;
         intitule.style.marginLeft = '2vw';
-        intitule.style.fontSize = '4vh';
-        firstDiv.appendChild(intitule);
+        intitule.style.fontSize = '2.5vh';
+        mainArticle.appendChild(intitule);
 
-        output.appendChild(firstDiv);
+        const description = document.createElement('p');
+        description.classList.add("task-description");
+        description.innerText = 'Description: ' + comments.value;
+        description.style.fontSize = '2vh';
+        description.style.margin = '.5vw 0';
+        if (comments.value) {
+            mainArticle.appendChild(description);
+        }
+
+        const dateAjout = document.createElement('p');
+        dateAjout.classList.add("task-date");
+        dateAjout.innerText = displayDateTime();
+        dateAjout.style.fontSize = '2vh';
+        mainArticle.appendChild(dateAjout);
+
+        output.appendChild(mainArticle);
+        deleteTask(mainArticle);
+        EditTask(mainArticle);
         newIntForId();
-
-        /*newOutput.appendChild(newDiv);
-        newButton.innerHTML = "<button style='padding: .3em;'>Supprimer</button>";
-        newButton.classList.add("eraser");
-        newTask.appendChild(newButton);
-        
-
-        newButton.addEventListener("click", function() {
-            if (newTask.classList.contains("active-tasks") == true) {
-                newOutput.removeChild(newTask);
-            }
-        })*/
     }
 }
+
+// Fonction pour effecer les champs renseignés dans l'intitulé et les commentaires
+function resetText() {
+    input.value = '';
+    comments.value = '';
+    return input.value, comments.value;
+};
+
+// Fonction pour supprimer une tâche
+function deleteTask(mainArticle) {
+    // Création du bouton pour supprimer une tâche
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add("delete-task");
+    deleteButton.setAttribute("type", "button");
+    deleteButton.innerText = "Supprimer";
+    deleteButton.style.padding = '.15em .5em';
+    deleteButton.style.margin = '.3vw 4px 0 0';
+    deleteButton.style.fontSize = '2vh';
+
+    deleteButton.addEventListener('click', () => {
+        mainArticle.remove();
+    });
+
+    mainArticle.appendChild(deleteButton);
+};
+
+// Fonction pour éditer une tâche
+function EditTask(mainArticle) {
+    // Bouton pour éditer une tâche
+    const editButton = document.createElement('button');
+    editButton.classList.add("task-editor");
+    editButton.setAttribute("type", "button");
+    editButton.innerText = "Editer";
+    editButton.style.padding = '.15em .5em';
+    editButton.style.fontSize = '2vh';
+
+    editButton.addEventListener('click', () => {});
+
+    mainArticle.appendChild(editButton);
+}
+
+/*editButton.addEventListener('click', () => {
+    button.innerText = "Appliquer les changements";
+    button.addEventListener('click', () => {
+        if ((input.value != intitule.innerText) || (comments.value != description.innerText.slice(13))) {
+
+        } else {
+
+        }
+    });
+});*/
