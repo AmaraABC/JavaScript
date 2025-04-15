043015d1089d3e020d70b11e1b00f6b494b8c2ff
AmaraABC <amara-noah.gakou@efrei.net>
2025-04-15 09:27:42 +0200

    Nouvelle branche

--- Desktop/Entraînement JavaScript/JavaScript Projects/To-Do list/script.js
+++ Desktop/Entraînement JavaScript/JavaScript Projects/To-Do list/script.js
@@ -1,37 +1,133 @@
-const newInput = document.getElementById("tasks");
-const newOutput = document.getElementById("tasks-output");
+// 3 constantes pour représenter l'intitulé de la tâche, son résumé et son ajout
+const input = document.getElementById("task");
+const comments = document.getElementById("comments");
+const output = document.getElementById("tasks-output");
+const button = document.getElementById("submit-button");
 
-const actualDate = new Date();
-const myDate = actualDate.toLocaleString('fr-FR',{
-    weekday: 'short',
-    day: 'numeric',
-    month: '2-digit',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
-})
+// Définition de la date et du temps
+function displayDateTime() {
+    const actualDate = new Date();
+    const myDate = actualDate.toLocaleString('fr-FR', {
+        weekday: 'short',
+        day: 'numeric',
+        month: '2-digit',
+        year: 'numeric'
+    });
 
+    const myTime = actualDate.toLocaleString('fr-FR', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    });
+
+    return `Date de création : ${myDate}, à ${myTime}`
+}
+
+// Fonction d'incrémentation pour différencier les identifiants de chaque tâche
+let idIncrement = 1;
+
+function newIntForId() {
+    return idIncrement++;
+}
+
+// Fonction pour l'ajout d'une nouvelle tâche
 function addTasks() {
-    if (newInput.value == '') {
-        alert("Vous devez au moins écrire quelque chose...");
+    if (input.value == '') {
+        alert("Vous devez au moins écrire quelque chose dans l'intitulé...");
     }
 
     else {
-        const newTask = document.createElement("span");
-        newTask.classList.add("active-tasks");
-        newTask.innerHTML = newInput.value + " - " + myDate;
-        newOutput.appendChild(newTask);
-        const newButton = document.createElement("button");
-        newButton.innerHTML = "<button style='padding: .3em;'>Supprimer</button>";
-        newButton.classList.add("eraser");
-        newTask.appendChild(newButton);
-        
+        // Définition d'une div et de sa classe
+        const mainArticle = document.createElement('div');
+        mainArticle.style.marginBottom = "1vw";
+        mainArticle.classList.add("active-tasks");
 
-        newButton.addEventListener("click", function() {
-            if (newTask.classList.contains("active-tasks") == true) {
-                newOutput.removeChild(newTask);
-            }
-        })
+        // Ajout de la tâche saisie auparavant
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute("id", `task-check-${idIncrement}`);
+        checkbox.setAttribute("name", `task-check-${idIncrement}`);
+        checkbox.setAttribute("type", "checkbox");
+        mainArticle.appendChild(checkbox);
+
+        const intitule = document.createElement('label');
+        intitule.classList.add("task-name");
+        intitule.setAttribute("for", `task-check-${idIncrement}`);
+        intitule.innerText = input.value;
+        intitule.style.marginLeft = '1vw';
+        intitule.style.fontSize = '2.5vh';
+        mainArticle.appendChild(intitule);
+
+        const description = document.createElement('p');
+        description.classList.add("task-description");
+        description.innerText = 'Description: ' + comments.value;
+        description.style.fontSize = '2vh';
+        description.style.margin = '.5vw 0';
+        if (comments.value) {
+            mainArticle.appendChild(description);
+        }
+
+        const dateAjout = document.createElement('p');
+        dateAjout.classList.add("task-date");
+        dateAjout.innerText = displayDateTime();
+        dateAjout.style.fontSize = '2vh';
+        mainArticle.appendChild(dateAjout);
+
+        output.appendChild(mainArticle);
+        resetText();
+        deleteTask(mainArticle);
+        editTask(mainArticle, intitule, description);
+        newIntForId();
     }
 }
+
+// Fonction pour effecer les champs renseignés dans l'intitulé et les commentaires
+function resetText() {
+    input.value = '';
+    comments.value = '';
+    return input.value, comments.value;
+};
+
+// Fonction pour editer une tâche enregistrée
+function editTask(mainArticle, intitule, description) {
+    // Création du bouton pour supprimer une tâche
+    const editButton = document.createElement('button');
+    editButton.classList.add("edit-task");
+    editButton.setAttribute("type", "button");
+    editButton.innerText = "Editer";
+    editButton.style.padding = '.15em .5em';
+    editButton.style.margin = '.3vw 4px 0 0';
+    editButton.style.fontSize = '2vh';
+
+    editButton.addEventListener('click', () => {
+        button.onclick = () => {
+            mainArticle.remove();
+            addTasks();
+        };
+        input.value = intitule.innerText;
+        if (description.value != "") {
+            comments.value = description.innerText.slice(13);
+            return comments.value;
+        };
+        return input.value;
+    });
+    
+    mainArticle.appendChild(editButton);
+}
+
+// Fonction pour supprimer une tâche
+function deleteTask(mainArticle) {
+    // Création du bouton pour supprimer une tâche
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add("delete-task");
+    deleteButton.setAttribute("type", "button");
+    deleteButton.innerText = "Supprimer";
+    deleteButton.style.padding = '.15em .5em';
+    deleteButton.style.margin = '.3vw 4px 0 0';
+    deleteButton.style.fontSize = '2vh';
+
+    deleteButton.addEventListener('click', () => {
+        mainArticle.remove();
+    });
+
+    mainArticle.appendChild(deleteButton);
+};
--- Downloads/DevOps/DevOps/src/main/java/com/example/DevOps/Car.java
+++ Downloads/DevOps/DevOps/src/main/java/com/example/DevOps/Car.java
@@ -1,4 +1,41 @@
 package com.example.DevOps;
 
 public class Car {
+   private String brand;
+   private String plateNumber;
+   private String price;
+
+    public Car(String brand) {
+        this.brand = brand;
+    }
+
+    public Car(String brand, String plateNumber, String price) {
+        this.brand = brand;
+        this.plateNumber = plateNumber;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
